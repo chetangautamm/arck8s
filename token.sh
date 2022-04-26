699905e0496bd95049ffb027d98693dc00f5e729
chetan <chetan.gautam@tcs.com>
2022-04-26 05:41:09 +0000

    opensips_yaml

--- token.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl create serviceaccount admin-user
-kubectl create clusterrolebinding admin-user-binding --clusterrole cluster-admin --serviceaccount default:admin-user
-SECRET_NAME=$(kubectl get serviceaccount admin-user -o jsonpath='{$.secrets[0].name}')
-TOKEN=$(kubectl get secret ${SECRET_NAME} -o jsonpath='{$.data.token}' | base64 -d | sed $'s/$/\\\n/g')
-echo $TOKEN > token.yml
